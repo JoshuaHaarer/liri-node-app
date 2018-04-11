7bf6366c037cc968f2039344c9f1def55f64dbea
joshua <joshuahaarer86@gmail.com>
2018-04-10 20:35:23 -0700

    starter code

--- liri.js
+++ liri.js
@@ -1,5 +1,22 @@
-var twitter = require("twitter");
-var spotify = require("spotify");
-var request = require("request");
-var dotenv = require("dotenv");
+var dotenv = require("dotenv").config();
 
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var request = require("request");
+
+var params = { screen_name: 'jmancoder' };
+
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (!error) {
+        console.log(tweets);
+    }
+});
+
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+});
